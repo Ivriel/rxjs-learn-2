ba630662a7830e3bc7b736675f0bf3307e265e5a
Ivriel Gunawan <gunawanivriel@gmail.com>
2025-08-25 13:46:18 +0700

    Observable basic learning material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RxjsBasicComponent } from './components/rxjs-basic/rxjs-basic.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'rxjs-basic',
+        component:RxjsBasicComponent
+    }
+];
---
+++ src/app/components/rxjs-basic/rxjs-basic.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { from, interval, Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-rxjs-basic',
+  imports: [],
+  templateUrl: './rxjs-basic.component.html',
+  styleUrl: './rxjs-basic.component.css'
+})
+export class RxjsBasicComponent {
+
+    cityList:string[] = ["Pune","Mumbai","Nagpur"];
+    cityList$ = of(["Pune","Mumbai","Nagpur"]);
+
+    cityList2$ = from(["Pune","Mumbai","Nagpur"]);
+
+    myInterval$ = interval(2000);
+
+    constructor(){
+
+      this.myInterval$.subscribe((res:number)=> {
+        console.log('Timer: ',res)
+      })
+
+      this.cityList$.subscribe((cityData:string[])=> {
+        debugger;
+        console.log(cityData)
+      })
+
+      this.cityList2$.subscribe((res:string)=> {
+        debugger;
+      })
+
+      const myObs$ = new Observable(value=> {
+        value.next("This is Demo Text")
+      })
+
+      myObs$.subscribe(res=> {
+        debugger;
+        console.log(res)
+      })
+    }
+}
